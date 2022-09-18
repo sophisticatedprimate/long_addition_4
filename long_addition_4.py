459954dafb71ce88a96a991104a0dfaa60eed651
SophisticatedPrimate <admin@sophisticatedprimate.com>
2022-09-17 20:22:10 -0500

    Add files via upload

---
+++ long_addition_4.py
@@ -0,0 +1,171 @@
+"""
+Check out the official python docs to learn more about python programming!
+https://docs.python.org/3/
+
+Refer to long_addition_1.py for further information on adding in python:
+https://github.com/sophisticatedprimate/long_addition_1/blob/main/long_addition_1.py
+
+To run the program: 
+    1. Open a terminal on a computer with python 3 installed (works with v2 as well)
+    2. Navigate to a directory containing this file (long_addition_1.py)
+    3. Run the program with the following command: python3 long_addition_1.py
+    4. A print out of the sums should appear after the cursor
+
+If error:
+    a. make sure python is installed (run: python3 --version)
+    b. make sure you have the file installed and in the current directory
+    c. if you are still having issues or see any general issues in the programming please report using the contact information at end of file.
+"""
+
+def add(a, b):
+    print("{0:<4} + {1:<4} = {2:<4}".format(a,b,a+b))
+
+
+# Problem 1  
+sum = 1164 + 1065 
+print("{0:<4} + {1:<4} = {2:<5}".format(1164,1065,sum))
+
+# Problem 2  
+sum = 225 + 978 
+print("{0:<4} + {1:<4} = {2:<5}".format(225,978,sum))
+
+# Problem 3  
+sum = 446 + 603 
+print("{0:<4} + {1:<4} = {2:<5}".format(446,603,sum))
+
+# Problem 4  
+sum = 942 + 750 
+print("{0:<4} + {1:<4} = {2:<5}".format(942,750,sum))
+
+# Problem 5  
+sum = 121 + 374 
+print("{0:<4} + {1:<4} = {2:<5}".format(121,374,sum))
+
+# Problem 6  
+sum = 368 + 918 
+print("{0:<4} + {1:<4} = {2:<5}".format(368,918,sum))
+
+# Problem 7  
+sum = 527 + 1154 
+print("{0:<4} + {1:<4} = {2:<5}".format(527,1154,sum))
+
+# Problem 8  
+sum = 387 + 1264 
+print("{0:<4} + {1:<4} = {2:<5}".format(387,1264,sum))
+
+# Problem 9  
+sum = 1297 + 651 
+print("{0:<4} + {1:<4} = {2:<5}".format(1297,651,sum))
+
+# Problem 10  
+sum = 743 + 1251 
+print("{0:<4} + {1:<4} = {2:<5}".format(743,1251,sum))
+
+# Problem 11  
+sum = 1112 + 1294 
+print("{0:<4} + {1:<4} = {2:<5}".format(1112,1294,sum))
+
+# Problem 12  
+sum = 137 + 130 
+print("{0:<4} + {1:<4} = {2:<5}".format(137,130,sum))
+
+# Problem 13  
+sum = 451 + 1101 
+print("{0:<4} + {1:<4} = {2:<5}".format(451,1101,sum))
+
+# Problem 14  
+sum = 337 + 824 
+print("{0:<4} + {1:<4} = {2:<5}".format(337,824,sum))
+
+# Problem 15  
+sum = 1049 + 251 
+print("{0:<4} + {1:<4} = {2:<5}".format(1049,251,sum))
+
+# Problem 16  
+sum = 528 + 119 
+print("{0:<4} + {1:<4} = {2:<5}".format(528,119,sum))
+
+# Problem 17  
+sum = 1248 + 607 
+print("{0:<4} + {1:<4} = {2:<5}".format(1248,607,sum))
+
+# Problem 18  
+sum = 575 + 373 
+print("{0:<4} + {1:<4} = {2:<5}".format(575,373,sum))
+
+# Problem 19  
+sum = 813 + 1103 
+print("{0:<4} + {1:<4} = {2:<5}".format(813,1103,sum))
+
+# Problem 20  
+sum = 1184 + 348 
+print("{0:<4} + {1:<4} = {2:<5}".format(1184,348,sum))
+
+# Problem 21  
+sum = 1094 + 414 
+print("{0:<4} + {1:<4} = {2:<5}".format(1094,414,sum))
+
+# Problem 22  
+sum = 461 + 916 
+print("{0:<4} + {1:<4} = {2:<5}".format(461,916,sum))
+
+# Problem 23  
+sum = 1127 + 1041 
+print("{0:<4} + {1:<4} = {2:<5}".format(1127,1041,sum))
+
+# Problem 24  
+sum = 927 + 653 
+print("{0:<4} + {1:<4} = {2:<5}".format(927,653,sum))
+
+# Problem 25  
+sum = 927 + 1208 
+print("{0:<4} + {1:<4} = {2:<5}".format(927,1208,sum))
+
+# Problem 26  
+sum = 601 + 1041 
+print("{0:<4} + {1:<4} = {2:<5}".format(601,1041,sum))
+
+# Problem 27  
+sum = 389 + 340 
+print("{0:<4} + {1:<4} = {2:<5}".format(389,340,sum))
+
+# Problem 28  
+sum = 606 + 429 
+print("{0:<4} + {1:<4} = {2:<5}".format(606,429,sum))
+
+# Problem 29  
+sum = 427 + 597 
+print("{0:<4} + {1:<4} = {2:<5}".format(427,597,sum))
+
+# Problem 30  
+sum = 671 + 1072 
+print("{0:<4} + {1:<4} = {2:<5}".format(671,1072,sum))
+
+# Problem 31  
+sum = 547 + 167 
+print("{0:<4} + {1:<4} = {2:<5}".format(547,167,sum))
+
+# Problem 32  
+sum = 438 + 311 
+print("{0:<4} + {1:<4} = {2:<5}".format(438,311,sum))
+
+# Problem 33  
+sum = 141 + 1123 
+print("{0:<4} + {1:<4} = {2:<5}".format(141,1123,sum))
+
+# Problem 34  
+sum = 759 + 415 
+print("{0:<4} + {1:<4} = {2:<5}".format(759,415,sum))
+
+# Problem 35  
+sum = 1225 + 508 
+print("{0:<4} + {1:<4} = {2:<5}".format(1225,508,sum))
+
+"""
+Contact information
+
+General Queries: admin@sophisticatedprimate.com
+Report errors, bugs, and problems at:
+    email:  bug@sophisticatedprimate.com
+    github: https://github.com/sophisticatedprimate/long_addition_1/issues
+"""
